3359a15ec2249e8d778029e4364c4f2fb595722f
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-02-18 20:35:49 +0200

    виправлення тексту

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,62 +1,71 @@
-import { Link, useNavigate } from 'react-router-dom';
-import Button from '../../components/Button/Button';
-import Headling from '../../components/Headling/Headling';
-import Input from '../../components/Input/Input';
-import styles from './Login.module.css';
-import { FormEvent, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispath, RootState } from '../../store/store';
-import { login, userActions } from '../../store/user.slice';
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import Headling from "../../components/Headling/Headling";
+import Input from "../../components/Input/Input";
+import styles from "./Login.module.css";
+import { FormEvent, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispath, RootState } from "../../store/store";
+import { login, userActions } from "../../store/user.slice";
 
 export type LoginForm = {
-	email: {
-		value: string;
-	};
-	password: {
-		value: string;
-	};
-}
+  email: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+};
 
 export function Login() {
-	const navigate = useNavigate();
-	const dispatch = useDispatch<AppDispath>();
-	const { jwt, loginErrorMessage } = useSelector((s: RootState) => s.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispath>();
+  const { jwt, loginErrorMessage } = useSelector((s: RootState) => s.user);
 
-	useEffect(() => {
-		if (jwt) {
-			navigate('/');
-		}
-	}, [jwt, navigate]);
+  useEffect(() => {
+    if (jwt) {
+      navigate("/");
+    }
+  }, [jwt, navigate]);
 
-	const submit = async (e: FormEvent) => {
-		e.preventDefault();
-		dispatch(userActions.clearLoginError());
-		const target = e.target as typeof e.target & LoginForm;
-		const { email, password } = target;
-		await sendLogin(email.value, password.value);
-	};
+  const submit = async (e: FormEvent) => {
+    e.preventDefault();
+    dispatch(userActions.clearLoginError());
+    const target = e.target as typeof e.target & LoginForm;
+    const { email, password } = target;
+    await sendLogin(email.value, password.value);
+  };
 
-	const sendLogin = async (email: string, password: string) => {
-		dispatch(login({ email, password }));
-	};
+  const sendLogin = async (email: string, password: string) => {
+    dispatch(login({ email, password }));
+  };
 
-	return <div className={styles['login']}>
-		<Headling>Вход</Headling>
-		{loginErrorMessage && <div className={styles['error']}>{loginErrorMessage}</div>}
-		<form className={styles['form']} onSubmit={submit}>
-			<div className={styles['field']}>
-				<label htmlFor="email">Ваш email</label>
-				<Input id="email" name='email' placeholder='Email' />
-			</div>
-			<div className={styles['field']}>
-				<label htmlFor="password">Ваш пароль</label>
-				<Input id="password" name='password' type="password" placeholder='Пароль' />
-			</div>
-			<Button appearence="big">Вход</Button>
-		</form>
-		<div className={styles['links']}>
-			<div>Нет акканута?</div>
-			<Link to="/auth/register">Зарегистрироваться</Link>
-		</div>
-	</div>;
+  return (
+    <div className={styles["login"]}>
+      <Headling>Вхід</Headling>
+      {loginErrorMessage && (
+        <div className={styles["error"]}>{loginErrorMessage}</div>
+      )}
+      <form className={styles["form"]} onSubmit={submit}>
+        <div className={styles["field"]}>
+          <label htmlFor="email">Ваш email</label>
+          <Input id="email" name="email" placeholder="Email" />
+        </div>
+        <div className={styles["field"]}>
+          <label htmlFor="password">Ваш пароль</label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Пароль"
+          />
+        </div>
+        <Button appearence="big">Вхід</Button>
+      </form>
+      <div className={styles["links"]}>
+        <div>Немає акканута?</div>
+        <Link to="/auth/register">Зареєструватися</Link>
+      </div>
+    </div>
+  );
 }
